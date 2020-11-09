2174e4e06878595d848d6e647b53833600453a8a
johnx <johx@sfu.ca>
2020-11-09 00:11:14 -0800

    added colour.py to store colour variables

---
+++ colour.py
@@ -0,0 +1,21 @@
+"""
+    This file contains some predefined colours
+"""
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+CREAM = (244, 244, 228)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (0, 111, 255)
+ORANGE = (255, 128, 0)
+PURPLE = (128, 0, 255)
+YELLOW = (255, 255, 0)
+GREY = (143, 143, 143)
+LIGHT_GREY = (190, 190, 190)
+BROWN = (186, 127, 50)
+DARK_GREEN = (0, 128, 0)
+DARKER_GREEN = (0, 50, 0)
+DARK_BLUE = (0, 0, 128)
